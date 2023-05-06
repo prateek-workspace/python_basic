0c87986373dad0dbc27721f017804dbeac97f3ff
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2023-05-06 17:40:55 +0000

    Created prg7.py

---
+++ Prg7.py
@@ -0,0 +1,11 @@
+#WAP to calculate SI.
+
+p = eval(input("Enter Principal Amount:  "))
+
+r = eval(input("Enter rate :  "))
+
+t = eval(input("Enter Time :  "))
+
+si = p*r*t / 100
+
+print("Simple Intrest is :  " ,si)
