c3dbbf46690568f39c5a5f439a2d539dd886bc21
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2023-05-06 17:31:54 +0000

    Created Prg6.py

---
+++ Prg6.py
@@ -0,0 +1,16 @@
+#WAP for swapping of two numbers.
+
+a = eval(input("Enter first number : "))
+
+b = eval(input("Enter second number : "))
+
+c = a
+
+a = b
+
+b = c
+
+print("Numbers after  swapping are : " , a , b)
+
+
+
