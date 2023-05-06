198f5c752366b05b31410cd96a31fde224a9f20b
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2023-05-06 17:08:10 +0000

    Created Prg3.py

---
+++ Prg3.py
@@ -0,0 +1,9 @@
+#WAP to find area of a Rectangle.
+
+l = eval(input("Enter Length of rectangle:-- "))
+
+b = eval(input("Enter Breadth of rectangle:-- "))
+
+area = l * b
+
+print("Area of Rectangle is : " ,area)
