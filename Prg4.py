e23bcecdec516c2305e7cdde5a2d9bd3ba3b22e6
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2023-05-06 17:18:55 +0000

    Created Prg4.py

---
+++ Prg4.py
@@ -0,0 +1,11 @@
+#WAP to covert distance from Km to m and cm.
+
+km = eval(input("Enter distance : "))
+
+m = km * 1000
+
+cm = m * 100
+
+print("Distance in m is : " ,m)
+
+print("Distance in cm is : " ,cm)
