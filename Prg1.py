59af41c9661f2b32610b09edd1fcc13ec85cc9e9
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2023-05-06 22:31:22 +0530

    Create Prg1.py

---
+++ Prg1.py
@@ -0,0 +1,20 @@
+#WAP to perform all arithematic operations on two numbers entered by user.
+
+a = eval(input("Enter First Number:--"))
+b = eval(input("Enter Second Number:--"))
+
+sum = a + b
+diff = a - b
+mul = a * b
+div = a / b
+mod = a % b
+
+print("Sum of a and b is:--" ,sum)
+
+print("Diffrence of a and b is:--" ,diff)
+
+print("Multiplication of a and b is:--" ,mul)
+
+print("Division of a and b is:--" ,div)
+
+print("Modulus of a and b is:--" ,mod)
